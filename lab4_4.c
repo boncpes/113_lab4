dce893f9240dd5ca2ccf896f3d2458d4dc1934d9
boncpes <78350722+boncpes@users.noreply.github.com>
2021-03-01 19:59:29 +0700

    4_4 final

--- lab4_4.c
+++ lab4_4.c
@@ -3,16 +3,26 @@
 #include<ctype.h>
 int main()
 {
-    int i,j,k,p=0;
+    int i,j,p=0,t=0;
     char c[11][4]= {"i","of","the","on","at","for","with","a","an","in","and"};
     char w[1000],b[20];
     char * pch;
-    scanf("%[^\n]s",w);
+    scanf("%1000[^\n]s",w);
     pch = strtok(w," ");
     while (pch != NULL)
     {
-        printf ("%s\n",b[0]);
+        for(i=0;i<11;i++)
+        {
+            if(strcmp(pch,c[i]))
+                    t++;
+        }
+        if(p==0||t==11)
+        {
+            printf ("%c",toupper(pch[0]));
+        }
         pch = strtok (NULL, " ");
+        t=0;
+        p++;
     }
     return 0;
 }
