ae54891d0d553bb689f77fb2f2b5f9620f1feb02
boncpes <78350722+boncpes@users.noreply.github.com>
2021-03-01 19:34:08 +0700

    lab4_4 strtok

--- lab4_4.c
+++ lab4_4.c
@@ -6,47 +6,13 @@ int main()
     int i,j,k,p=0;
     char c[11][4]= {"i","of","the","on","at","for","with","a","an","in","and"};
     char w[1000],b[20];
-    scanf("%1000s[^\n]",w);
+    scanf("%s[^\n]",w);
     j=0;
-    for(i=0; i<strlen(w); i++)
+    char *token = strtok(w," ");
+    while(token != NULL)
     {
-        if(w[i]==' ')
-        {
-            if(i==0)
-            {
-                for(k=0; k<11; k++)
-                {
-                    if(strcmp(b,c[k])==0)
-                    {
-                        printf("%c",toupper(b[0]));
-                        strcpy(b, "");
-                        j=0;
-                    }
-                }
-            }
-            else
-            {
-                for(k=0; k<11; k++)
-                {
-                    if(strcmp(b,c[k])==0)
-                    {
-                        p++;
-                    }
-                }
-                if(p>0)
-                {
-                    printf("%c",toupper(b[0]));
-                    p=0;
-                    strcpy(b, "");
-                    j=0;
-                }
-            }
-        }
-        else
-        {
-            b[j]=w[i];
-            j++;
-        }
-        return 0;
+         printf("%s\n",token);
+         token = strtok(NULL," ");
     }
+    return 0;
 }
