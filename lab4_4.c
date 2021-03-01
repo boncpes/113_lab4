db50d0251b74f2de00f417cf17b6e8e1b75b2a17
boncpes <78350722+boncpes@users.noreply.github.com>
2021-03-01 18:33:44 +0700

    lab4_4 not yet

--- lab4_4.c
+++ lab4_4.c
@@ -3,32 +3,50 @@
 #include<ctype.h>
 int main()
 {
-    int i,k=0;
+    int i,j,k,p=0;
+    char c[11][4]= {"i","of","the","on","at","for","with","a","an","in","and"};
     char w[1000],b[20];
     scanf("%1000[^\n]s",w);
-    int j=0;
+    j=0;
     for(i=0; i<strlen(w); i++)
     {
-        if(w[i]!=' ')
-        {
-            b[j]=w[i];
-            j++;
-        }
-        else
+        if(w[i]==' ')
         {
             if(i==0)
             {
-                if(strcmp(b,i)||strcmp(b,of))
+                for(k=0; k<11; k++)
                 {
-                    printf("%c",toupper(b[0]));
+                    if(strcmp(b,c[k])==0)
+                    {
+                        printf("%c",toupper(b[0]));
+                        strcpy(b, "");
+                        j=0;
+                    }
                 }
             }
             else
             {
-                printf("")
+                for(k=0; k<11; k++)
+                {
+                    if(strcmp(b,c[k])==0)
+                    {
+                        p++;
+                    }
+                }
+                if(p>0)
+                {
+                    printf("%c",toupper(b[0]));
+                    p=0;
+                    strcpy(b, "");
+                    j=0;
+                }
             }
-
+        }
+        else
+        {
+            b[j]=w[i];
+            j++;
+        }
+        return 0;
     }
-
-    return 0;
 }
